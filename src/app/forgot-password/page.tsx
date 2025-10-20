2a3dde793921013a465ccc5157f47dfa44431717
darcrand <darcrandex@gmail.com>
2025-10-20 17:45:58 +0800

    🌈 style(app): 改下样式

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -3,6 +3,7 @@
 import { sendResetPasswordEmail } from '@/actions/user'
 import { useMutation } from '@tanstack/react-query'
 import { App, Button, Form, Input } from 'antd'
+import Link from 'next/link'
 
 export default function ForgotPassword() {
   const { message } = App.useApp()
@@ -23,19 +24,29 @@ export default function ForgotPassword() {
   })
 
   return (
-    <>
+    <section className="from-primary flex h-screen items-center justify-center bg-linear-180 to-white">
       <div className="w-xl bg-white p-4 shadow-xl">
-        <Form onFinish={submitMutation.mutate}>
+        <h1 className="text-primary mb-8 text-center text-3xl font-bold italic">Forgot Password</h1>
+
+        <Form layout="vertical" onFinish={submitMutation.mutate}>
           <Form.Item name="email" label="Email" rules={[{ required: true, message: 'Please input your email!' }]}>
             <Input />
           </Form.Item>
           <Form.Item>
-            <Button type="primary" htmlType="submit" loading={submitMutation.isPending}>
-              Send Reset Email
+            <Button type="primary" block htmlType="submit" loading={submitMutation.isPending}>
+              Send
             </Button>
           </Form.Item>
         </Form>
+
+        <footer className="my-4 text-center">
+          <p>
+            <Link href="/login" className="mx-2 text-blue-600 underline">
+              back to login
+            </Link>
+          </p>
+        </footer>
       </div>
-    </>
+    </section>
   )
 }
--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -3,6 +3,7 @@
 import { resetPassword } from '@/actions/user'
 import { useMutation } from '@tanstack/react-query'
 import { App, Button, Form, Input } from 'antd'
+import Link from 'next/link'
 import { useRouter, useSearchParams } from 'next/navigation'
 
 export default function ResetPassword() {
@@ -28,47 +29,55 @@ export default function ResetPassword() {
   })
 
   return (
-    <>
-      <div className="mx-auto mt-4 w-xl">
-        <p>sign: {sign}</p>
-      </div>
+    <section className="from-primary flex h-screen items-center justify-center bg-linear-180 to-white">
+      <div className="w-xl bg-white p-4 shadow-xl">
+        <h1 className="text-primary mb-8 text-center text-3xl font-bold italic">Reset Password</h1>
 
-      <Form onFinish={submit.mutate}>
-        <Form.Item
-          name="newPassword"
-          label="New Password"
-          rules={[
-            { required: true, message: 'Please enter your new password' },
-            { min: 6, message: 'Password length must be at least 6 characters' }
-          ]}
-        >
-          <Input.Password maxLength={30} allowClear />
-        </Form.Item>
+        <Form layout="vertical" onFinish={submit.mutate}>
+          <Form.Item
+            name="newPassword"
+            label="New Password"
+            rules={[
+              { required: true, message: 'Please enter your new password' },
+              { min: 6, message: 'Password length must be at least 6 characters' }
+            ]}
+          >
+            <Input.Password maxLength={30} allowClear />
+          </Form.Item>
 
-        <Form.Item
-          name="confirmPassword"
-          label="Confirm New Password"
-          rules={[
-            { required: true, message: 'Please confirm your new password' },
-            ({ getFieldValue }) => ({
-              validator(_, value) {
-                if (!value || getFieldValue('newPassword') === value) {
-                  return Promise.resolve()
+          <Form.Item
+            name="confirmPassword"
+            label="Confirm New Password"
+            rules={[
+              { required: true, message: 'Please confirm your new password' },
+              ({ getFieldValue }) => ({
+                validator(_, value) {
+                  if (!value || getFieldValue('newPassword') === value) {
+                    return Promise.resolve()
+                  }
+                  return Promise.reject(new Error('Passwords do not match'))
                 }
-                return Promise.reject(new Error('Passwords do not match'))
-              }
-            })
-          ]}
-        >
-          <Input.Password maxLength={30} allowClear />
-        </Form.Item>
+              })
+            ]}
+          >
+            <Input.Password maxLength={30} allowClear />
+          </Form.Item>
 
-        <Form.Item>
-          <Button type="primary" htmlType="submit" loading={submit.isPending}>
-            Reset Password
-          </Button>
-        </Form.Item>
-      </Form>
-    </>
+          <Form.Item>
+            <Button type="primary" block htmlType="submit" loading={submit.isPending}>
+              Reset
+            </Button>
+          </Form.Item>
+        </Form>
+
+        <footer className="my-4 text-center">
+          <p>
+            <Link href="/login" className="mx-2 text-blue-600 underline">
+              back to login
+            </Link>
+          </p>
+        </footer>
+      </div>
+    </section>
   )
 }
