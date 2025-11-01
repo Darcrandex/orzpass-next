c7a9e0a56fcd68ded514db3f9c3ccdaf2676d01e
darcrand <darcrandex@gmail.com>
2025-11-01 23:43:04 +0800

    🎈 perf(detail): add loading

--- src/app/(main)/pwd/[id]/page.tsx
+++ src/app/(main)/pwd/[id]/page.tsx
@@ -2,18 +2,22 @@
 
 import PasswordForm from '@/components/PasswordForm'
 import PasswordRemove from '@/components/PasswordRemove'
-import { Breadcrumb } from 'antd'
+import { useIsFetching } from '@tanstack/react-query'
+import { Breadcrumb, Spin } from 'antd'
 import { useParams } from 'next/navigation'
 
 export default function PwdDetail() {
   const { id } = useParams<{ id: string }>()
+  const isLoading = useIsFetching() > 0
   return (
     <>
       <Breadcrumb items={[{ title: 'Home' }, { title: 'Passwords' }, { title: 'Detail' }]}></Breadcrumb>
 
       <div className="mx-auto mt-4 w-xl">
-        <PasswordForm id={id} />
-        <PasswordRemove id={id} className="mt-20" />
+        <Spin spinning={isLoading}>
+          <PasswordForm id={id} />
+          <PasswordRemove id={id} className="mt-20" />
+        </Spin>
       </div>
     </>
   )
